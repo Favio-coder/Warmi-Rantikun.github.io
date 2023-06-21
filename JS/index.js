9e26ecb6a3a5f62c237a04bc94d72cded0e9427f
Favio Asturimac <faviusam@gmail.com>
2023-06-21 01:00:00 -0500

    Subiendo la versión v0.3

--- JS/index.js
+++ JS/index.js
@@ -46,3 +46,43 @@ const cerrarMenu = (boton, overlay) =>{
     boton.remove();
 }
 }
+
+function mensajeAleatorioMate(){
+  let mensajeAleatorio = aleatorio(1,5)
+
+  if (mensajeAleatorio  == 1) {
+    mensaje = 'La magia ancestral de plasmar recuerdos'
+
+  } else if (mensajeAleatorio  == 2) {
+    mensaje = "A través de la artesanía, preservamos nuestra cultura y herencia."
+
+  } else if (mensajeAleatorio  == 3) {
+    mensaje = "Los recuerdos perduran en la delicadeza de la artesanía."
+
+  } else if (mensajeAleatorio  == 4) {
+    mensaje = "La artesanía es el puente que une nuestro pasado y nuestra herencia cultural."
+
+  } else if (mensajeAleatorio == 5) {
+    mensaje = "En cada pieza artesanal, se refleja la riqueza de nuestra cultura y herencia."
+
+  } else {
+    mensaje = "Los recuerdos se entrelazan con la artesanía, creando una herencia viva."
+  }
+
+  crearMensaje()
+}
+
+
+function crearMensaje(){
+  let sectionMensajes = document.getElementById("mensajes");
+
+  let parrafo= document.createElement("p")
+  parrafo.innerHTML = mensaje
+
+  sectionMensajes.appendChild(parrafo)
+}
+
+
+function aleatorio(min,max){return Math.floor(Math.random()*(max-min+1)+min)}
+
+window.addEventListener('load', mensajeAleatorioMate)

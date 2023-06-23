08da28febb97ec3ec44139c5b6e3d186d1e78d9a
Favio Asturimac <faviusam@gmail.com>
2023-06-23 07:11:23 -0500

    subiendo carrusel y modificaciones

--- JS/index.js
+++ JS/index.js
@@ -86,3 +86,26 @@ function crearMensaje(){
 function aleatorio(min,max){return Math.floor(Math.random()*(max-min+1)+min)}
 
 window.addEventListener('load', mensajeAleatorioMate)
+
+/*boddy*/
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
